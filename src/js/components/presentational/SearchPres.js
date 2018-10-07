597e8f8d6d975d6b55a7c332f60ab6e8d523f5c1
Alexander Cherry <acherry125@gmail.com>
2018-10-06 21:23:23 -0400

    more landing page styles

--- src/js/components/presentational/SearchPres.js
+++ src/js/components/presentational/SearchPres.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { SearchWrapper, SearchTitle, SearchInnerWrapper, SearchLabel } from 'components/styled/styles';
+import { SearchWrapper, SearchTitle, SearchInnerWrapper, SearchLabel, SearchInput } from 'components/styled/styles';
 
 export default class SearchPres extends React.Component {
   render() {
@@ -11,7 +11,7 @@ export default class SearchPres extends React.Component {
         <SearchTitle>Search the world's Data</SearchTitle>
         <SearchInnerWrapper>
           <SearchLabel htmlFor="search-bar">Search:</SearchLabel>
-          <input id="search-bar" placeholder={searchPlaceholder} />
+          <SearchInput id="search-bar" placeholder={searchPlaceholder} />
         </SearchInnerWrapper>
       </SearchWrapper>
     )
--- src/js/components/styled/styles.js
+++ src/js/components/styled/styles.js
@@ -2,39 +2,42 @@ import styled from 'styled-components';
 
 // Create a Title component that'll render an <h1> tag with some styles
 export const Title = styled.h1`
-  text-align: center;
   color: #000;
 `;
 
 // Create a Wrapper component that'll render a <section> tag with some styles
 export const Main = styled.main`
-  padding: 4em;
+  padding: 1.25em;
   background: var(--blue-gray);
   min-height: 100vh;
+  @media only screen and (min-width: 60em) {
+    padding: 4em;
+  }
 `;
 
 export const Content = styled.section`
-  background: var(--off-white);
-  padding: 2em 0.25em;
+  background: var(--white);
+  padding: 2em 2em;
+  box-shadow: 1px 1px 3px 1px var(--gray);
+  border-radius: 1px;
 `;
 
 export const SearchWrapper = styled.div`
   display: flex;
   flex-flow: column nowrap;
-  align-items: center;
+  align-items: flex-start;
 `;
 
 export const SearchTitle = styled.h2`
-  text-align: center;
   color: #000;
+  align-self: flex-start;
 `;
 
 export const SearchInnerWrapper = styled.div`
   display: flex;
-  flex-flow: row wrap;
-  align-items: center;
-  justify-content: space-between;
-  width: 15em;
+  flex-flow: column nowrap;
+  align-items: flex-start;
+  width: 100%;
 `;
 
 export const SearchLabel = styled.label`
@@ -42,5 +45,8 @@ export const SearchLabel = styled.label`
 `;
 
 export const SearchInput = styled.input`
-  
+  padding: 0.5em;
+  width: 100%;
+  max-width: 25rem;
+  font-size: 2em;
 `;
