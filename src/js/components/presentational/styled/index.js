bcadd82b1a9bddb9dbd414af234deff70e4b731d
Alexander Cherry <acherry125@gmail.com>
2019-01-06 19:53:52 -0500

    remove unneeded files

--- src/js/components/presentational/styled/index.js
+++ src/js/components/presentational/styled/index.js
@@ -1,3 +1,1 @@
 export * from './general';
-export * from './search';
-export * from './results';
--- src/js/components/presentational/styled/results.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const Result = styled.div`
-  background: var(--gray);
-  padding: 1em;
-  margin: 0.25em 0;
-`;
